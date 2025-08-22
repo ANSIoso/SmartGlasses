d4f29908dc689b453c5ae14cf8aa1262e5b6122b
ANSIoso <92176296+ANSIoso@users.noreply.github.com>
2025-08-22 23:55:04 +0200

    code clean up

--- DeviceScreen/DeviceScreen.ino
+++ DeviceScreen/DeviceScreen.ino
@@ -9,36 +9,35 @@
 #include "MenuLogic.h"
 #include "MenuView.h"
 
+
+// ====== COMUNICATION ======
+// file stream
 SoftwareSerial mySerial(6, 7); // RX, TX
-
-U8G2_SH1107_PIMORONI_128X128_1_4W_HW_SPI u8g2(U8G2_R1, /* cs=*/2, /* dc=*/3, /* reset=*/4);
-
+String data;
+JsonDocument doc;
 // baud rate info
 # define BR_COMUNICATION 9600 // to talk with esp32
 # define BR_DEBUG 57600       // to check device outputs
 
+// ====== VISUALIZATION ======
+U8G2_SH1107_PIMORONI_128X128_1_4W_HW_SPI u8g2(U8G2_R1, /* cs=*/2, /* dc=*/3, /* reset=*/4);
 // image settings
 # define IMG_HEIGHT 480
 # define IMG_WIDTH 640
-
 // screen settings
 # define SCREEN_HEIGHT 128
 # define SCREEN_WIDTH 128
 
-const int screenCenterX = SCREEN_WIDTH / 2;   // 64
-const int screenCenterY = SCREEN_HEIGHT / 2;  // 64
-const int radarRadius = 40;  // radius for arrow placement
-
-// buttons
+// ====== NAVIGATION ======
+// logic
+MenuLogic screenMenuL(MenuBitmaps::ICON_COUNT);
+int menu_btn_clicked = 0;
+// buttons pins
 #define BUTTON_UP 9
 #define BUTTON_SELECT 5
 #define BUTTON_DOWN 8
 
-String data;
-JsonDocument doc;
 
-int menu_btn_clicked = 0;
-MenuLogic screenMenuL(MenuBitmaps::ICON_COUNT);
 
 void setup() {
   mySerial.begin(BR_COMUNICATION);
@@ -59,6 +58,8 @@ void setup() {
 }
 
 void loop() {
+
+  // ---- handle menù navigation (via buttons) ----
   if((menu_btn_clicked == 0) && (digitalRead(BUTTON_SELECT) == LOW)){
 		screenMenuL.toggleMenu();
 		menu_btn_clicked = 1;
@@ -74,44 +75,65 @@ void loop() {
   if(digitalRead(BUTTON_DOWN) == LOW)
     screenMenuL.previousItem();
 
-  getObjs();
-  
-	if(screenMenuL.isMenuActive()){
-    int item_selected = screenMenuL.getSelectedItem();
-    int item_previous = screenMenuL.getPreviousItem();
-    int item_next = screenMenuL.getNextItem();
-
-    int objCount = 0;
-
-    for(int i = 0; i < doc.size(); i++) {
-      JsonObject obj = doc[i];
-
-      if(obj["class"] == MenuBitmaps::object_tipes[screenMenuL.getSelectedItem()])
-        objCount++;
-    }
-
-    DrawMenu(item_selected, item_previous, item_next, objCount);
-  }else
-    visualizeObjects();
-}
-
-void getObjs(){
-  if (!mySerial.available())
+  // ---- handle comunication with esp ----  
+  if(!getObjs())
     return;
 
+  
+  // ---- handle screen visualizzation ----
+	if(screenMenuL.isMenuActive())
+    DrawMenu();
+  else
+    VisualizeObjects();
+}
+
+// ======== OBJ UTILS ========
+bool getObjs(){
+  // check if there are data available to receive from esp
+  if (!mySerial.available())
+    return false;
+
+  // if available get the data
   data = mySerial.readStringUntil('\n');
+
+  // if data are not useful return error
+  if (data == "")
+    return false;
+
+  // convert the data string in a json file to access to single elements
   DeserializationError error = deserializeJson(doc, data);
   if (error != DeserializationError::Ok)
-    return;
+  return false;
+
+  // if all went good return success
+  return true;
 }
 
-void visualizeObjects(){  
-  if (!mySerial.available())
-    return;
+int CountClassObjects(char className[]){
+  int objCount = 0;
+
+  // going for all the found objects
+  for(int i = 0; i < doc.size(); i++) {
+    JsonObject obj = doc[i];
+    
+    // count all the istances of the specified class
+    if(obj["class"] == className)
+      objCount++;
+  }
+
+  return objCount;
+}
+
+// ======== DRAWING METODS ========
+void VisualizeObjects(){  
+  const int screenCenterX = SCREEN_WIDTH / 2;   
+  const int screenCenterY = SCREEN_HEIGHT / 2;  
+  const int radarRadius = 40;  // radius for arrow placement
 
   u8g2.firstPage();
   do {    
 
+    // draw radar contour
     u8g2.drawCircle(screenCenterX, screenCenterY, radarRadius-2, U8G2_DRAW_ALL);
     u8g2.drawCircle(screenCenterX, screenCenterY, radarRadius-3, U8G2_DRAW_ALL);
     u8g2.drawCircle(screenCenterX, screenCenterY, radarRadius+2, U8G2_DRAW_ALL);
@@ -121,6 +143,7 @@ void visualizeObjects(){
     for(int i = 0; i < doc.size(); i++) {
       JsonObject obj = doc[i];
 
+      // check that the istances are of the specified class      
       if(obj["class"] != MenuBitmaps::object_tipes[screenMenuL.getSelectedItem()])
         continue;
      
@@ -142,10 +165,12 @@ void visualizeObjects(){
       // Optional: Add distance indication with different arrow sizes
       float distance = sqrt(relativeX*relativeX + relativeY*relativeY);
 
-      // Draw directional arrow
-      if(distance >= radarRadius - 5)
+      // Based on the distance from the center
+      if(distance >= radarRadius - 5){
+        // Draw directional arrow
         drawDirectionalArrow(arrowX, arrowY, angle, (SCREEN_HEIGHT - distance)/3);
-      else{
+      }else{
+        // Draw the precise location
         long x = (objX * SCREEN_WIDTH) / IMG_WIDTH;
         long y = (objY * SCREEN_HEIGHT) / IMG_HEIGHT;
       
@@ -186,36 +211,40 @@ void drawDirectionalArrow(int x, int y, float angle, int arrowSize) {
   u8g2.drawTriangle(tipX, tipY, baseX1, baseY1, baseX2, baseY2);
 }
 
-void DrawMenu(int item_selected, int item_previous, int item_next, int found_objects){
-  if (!mySerial.available())
-    return;
+void DrawMenu(){
+  int item_selected = screenMenuL.getSelectedItem();
+  int item_previous = screenMenuL.getPreviousItem();
+  int item_next = screenMenuL.getNextItem();
+
+  // count all the istances of the specified class and prepare visualization string
+  int objCount = CountClassObjects(MenuBitmaps::object_tipes[screenMenuL.getSelectedItem()]);
+  String foundInfo = "Found " + String(objCount);
 
   u8g2.firstPage();
   do {    
-    u8g2.setBitmapMode(1);  // imposto la modalità di disegno per "combinare" le bitmap
+    u8g2.setBitmapMode(1);  // set drawing mode to "combine" the bitmaps
     
-    // === disegno gli elementi dell'interfaccia ===
-    // - dashboard (ospiterà le icone delle classi)
-    u8g2.drawXBMP(0, 29, 128, 56, MenuBitmaps::epd_bitmap_dashboard);
+    // === draw the interface elements ===
+    // - dashboard (to fit the classes icons)
+    u8g2.drawXBMP(40, 33, 48, 48, MenuBitmaps::epd_bitmap_dashboard);
 
-    // - scrollbar (per mostrare posizione nella lista delle classi)
+    // - scrollbar (to show the progression in the classes list)
           int scrollbarPos = 5 + ((128/MenuBitmaps::ICON_COUNT) * item_selected);
     u8g2.drawXBMP(0, 6, 128, 8, MenuBitmaps::epd_bitmap_scrollbar);
     u8g2.drawXBMP(scrollbarPos, 6, 24, 8, MenuBitmaps::epd_bitmap_scrollbar_handle);
 
-    // === "popolo" l'interfaccia
-    // - disegno la classe attualmente selezionata
-    // - disegno le classi prossime a quella attualmente selezionata
+    // === fill the interface
+    // - with the actual selected class
+    // - and the previos and next one
     u8g2.drawXBMP(6, 42, 32, 32, MenuBitmaps::icon_bitmaps[item_previous]);
     u8g2.drawXBMP(49, 42, 32, 32, MenuBitmaps::icon_bitmaps[item_selected]);
     u8g2.drawXBMP(92, 42, 32, 32, MenuBitmaps::icon_bitmaps[item_next]);
 
-    // - scrivo il numero di istanze trovate per una determinata classe   
-    String objCount = "Found " + String(found_objects);
+    // - draw the number of istance for the selected class
     u8g2.setFont(u8g2_font_crox5hb_tf);
-    u8g2.drawStr(18, 110, objCount.c_str());
+    u8g2.drawStr(18, 110, foundInfo.c_str());
     
-    u8g2.setBitmapMode(0);
+    u8g2.setBitmapMode(0); // set drawing mode to normal
 
   } while (u8g2.nextPage());
 }
